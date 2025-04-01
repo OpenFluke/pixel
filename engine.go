d90881a1e3bf032a19496389e33581df58923308
OpenFluke <openfluke@hotmail.com>
2025-04-01 15:26:04 +1100

    looks like a lama lol now just need to link them together

--- engine.go
+++ engine.go
@@ -18,6 +18,16 @@ const (
 
 type Message map[string]interface{}
 
+type Cube struct {
+	Name     string
+	Position []float64
+}
+
+var (
+	globalCubeList []string
+	cubeListMutex  sync.Mutex
+)
+
 func sendJSONMessage(conn net.Conn, msg Message) error {
 	data, err := json.Marshal(msg)
 	if err != nil {
@@ -46,28 +56,25 @@ func readResponse(conn net.Conn) (string, error) {
 	return strings.TrimSpace(full), nil
 }
 
-type Cube struct {
-	Name     string
-	Position []float64
-}
-
-func handleCube(cube Cube, wg *sync.WaitGroup) {
+func spawnCube(cube Cube, wg *sync.WaitGroup) {
 	defer wg.Done()
 	conn, err := net.Dial("tcp", serverAddr)
 	if err != nil {
-		fmt.Println("[Cube] Failed to connect:", err)
+		fmt.Println("[Spawn] Failed to connect:", err)
 		return
 	}
 	defer conn.Close()
+
 	if _, err := conn.Write([]byte(authPass + delimiter)); err != nil {
-		fmt.Println("[Cube] Auth write error:", err)
+		fmt.Println("[Spawn] Auth write error:", err)
 		return
 	}
 	_, err = readResponse(conn)
 	if err != nil {
-		fmt.Println("[Cube] Failed to read auth response:", err)
+		fmt.Println("[Spawn] Failed to read auth response:", err)
 		return
 	}
+
 	spawn := Message{
 		"type":      "spawn_cube",
 		"cube_name": cube.Name,
@@ -76,49 +83,79 @@ func handleCube(cube Cube, wg *sync.WaitGroup) {
 		"is_base":   true,
 	}
 	if err := sendJSONMessage(conn, spawn); err != nil {
-		fmt.Println("[Cube] Failed to spawn cube:", err)
+		fmt.Println("[Spawn] Failed to spawn cube:", err)
 		return
 	}
+
 	fullCubeName := cube.Name + "_BASE"
-	time.Sleep(1 * time.Second)
-	unfreeze := Message{
-		"type":      "freeze_cube",
-		"cube_name": fullCubeName,
-		"freeze":    false,
+	cubeListMutex.Lock()
+	globalCubeList = append(globalCubeList, fullCubeName)
+	cubeListMutex.Unlock()
+}
+
+func unfreezeAllCubes() {
+	var wg sync.WaitGroup
+	for _, cube := range globalCubeList {
+		wg.Add(1)
+		go func(name string) {
+			defer wg.Done()
+			conn, err := net.Dial("tcp", serverAddr)
+			if err != nil {
+				fmt.Println("[Unfreeze] Failed to connect:", err)
+				return
+			}
+			defer conn.Close()
+
+			if _, err := conn.Write([]byte(authPass + delimiter)); err != nil {
+				return
+			}
+			_, _ = readResponse(conn)
+
+			unfreeze := Message{
+				"type":      "freeze_cube",
+				"cube_name": name,
+				"freeze":    false,
+			}
+			sendJSONMessage(conn, unfreeze)
+		}(cube)
 	}
-	if err := sendJSONMessage(conn, unfreeze); err != nil {
-		fmt.Println("[Cube] Failed to unfreeze cube:", err)
-		return
+	wg.Wait()
+}
+
+func despawnAllCubes() {
+	var wg sync.WaitGroup
+	for _, cube := range globalCubeList {
+		wg.Add(1)
+		go func(name string) {
+			defer wg.Done()
+			conn, err := net.Dial("tcp", serverAddr)
+			if err != nil {
+				fmt.Println("[Despawn] Failed to connect:", err)
+				return
+			}
+			defer conn.Close()
+
+			if _, err := conn.Write([]byte(authPass + delimiter)); err != nil {
+				return
+			}
+			_, _ = readResponse(conn)
+
+			despawn := Message{
+				"type":      "despawn_cube",
+				"cube_name": name,
+			}
+			sendJSONMessage(conn, despawn)
+		}(cube)
 	}
-	time.Sleep(1 * time.Second)
-	freeze := Message{
-		"type":      "freeze_cube",
-		"cube_name": fullCubeName,
-		"freeze":    true,
-	}
-	if err := sendJSONMessage(conn, freeze); err != nil {
-		fmt.Println("[Cube] Failed to freeze cube:", err)
-		return
-	}
-	time.Sleep(2 * time.Second)
-	despawn := Message{
-		"type":      "despawn_cube",
-		"cube_name": fullCubeName,
-	}
-	if err := sendJSONMessage(conn, despawn); err != nil {
-		fmt.Println("[Cube] Failed to despawn cube:", err)
-		return
-	}
+	wg.Wait()
 }
 
 func main() {
 	cubeGroups := [][]Cube{
-		// Ears (top layer)
 		{
 			{Name: "rightear", Position: []float64{0, 130, 0}},
 			{Name: "leftear", Position: []float64{2, 130, 0}},
 		},
-		// Head (2x2 block below ears)
 		{
 			{Name: "head1", Position: []float64{0, 128.9, 0}},
 			{Name: "head2", Position: []float64{1, 128.9, 0}},
@@ -137,11 +174,9 @@ func main() {
 			{Name: "leftmouth", Position: []float64{0.5, 128.9, -1}},
 			{Name: "rightmouth", Position: []float64{1.5, 128.9, -1}},
 		},
-		// Neck (centered under head)
 		{
 			{Name: "neck", Position: []float64{1, 126.9, 0.5}},
 		},
-		// Body
 		{
 			{Name: "body1", Position: []float64{0, 125.9, 0}},
 			{Name: "body2", Position: []float64{1, 125.9, 0}},
@@ -155,7 +190,6 @@ func main() {
 			{Name: "body10", Position: []float64{0, 124.9, 1}},
 			{Name: "body11", Position: []float64{1, 124.9, 1}},
 			{Name: "body12", Position: []float64{2, 124.9, 1}},
-
 			{Name: "body13", Position: []float64{0, 125.9, 2}},
 			{Name: "body14", Position: []float64{1, 125.9, 2}},
 			{Name: "body15", Position: []float64{2, 125.9, 2}},
@@ -169,15 +203,48 @@ func main() {
 			{Name: "body23", Position: []float64{1, 124.9, 3}},
 			{Name: "body24", Position: []float64{2, 124.9, 3}},
 		},
+		{
+			{Name: "leftbackleg1", Position: []float64{2, 123.7, 3}},
+			{Name: "leftbackknee1", Position: []float64{2, 122.5, 3}},
+			{Name: "leftbackleg2", Position: []float64{2, 121.3, 3}},
+		},
+		{
+			{Name: "leftfrontleg1", Position: []float64{2, 123.7, 0}},
+			{Name: "leftfrontknee1", Position: []float64{2, 122.5, 0}},
+			{Name: "leftfrontleg2", Position: []float64{2, 121.3, 0}},
+		},
+		{
+			{Name: "rightbackleg1", Position: []float64{0, 123.7, 3}},
+			{Name: "rightbackknee1", Position: []float64{0, 122.5, 3}},
+			{Name: "rightbackleg2", Position: []float64{0, 121.3, 3}},
+		},
+		{
+			{Name: "rightfrontleg1", Position: []float64{0, 123.7, 0}},
+			{Name: "rightfrontknee1", Position: []float64{0, 122.5, 0}},
+			{Name: "rightfrontleg2", Position: []float64{0, 121.3, 0}},
+		},
+		{
+			{Name: "tail1", Position: []float64{1, 127.2, 3}},
+			{Name: "tail2", Position: []float64{1, 128.4, 3}},
+			{Name: "tail3", Position: []float64{1, 129.6, 3}},
+		},
 	}
 
 	var wg sync.WaitGroup
 	for _, group := range cubeGroups {
 		for _, cube := range group {
 			wg.Add(1)
-			go handleCube(cube, &wg)
+			go spawnCube(cube, &wg)
 		}
 	}
 	wg.Wait()
-	time.Sleep(5 * time.Second)
+
+	fmt.Println("Spawned all cubes.")
+	unfreezeAllCubes()
+
+	fmt.Println("Waiting 3 seconds before despawning...")
+	time.Sleep(3 * time.Second)
+
+	despawnAllCubes()
+	fmt.Println("Despawned all cubes.")
 }
